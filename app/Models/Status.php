cef651a16cd44684807ad5434c0f670a96598d23
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-09-18 22:07:43 +0545

    Softdelete added in models

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,14 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Status extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     const DEFAULT = 1;
-    
+
     protected $table = 'status';
-    
+
     protected $fillable = ['title'];
 }
