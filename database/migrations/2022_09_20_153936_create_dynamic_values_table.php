7953622e8946fcec994564bfd424f2be2f1c93cc
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-09-22 17:22:55 +0545

    dynamic_values migration changed

--- database/migrations/2022_09_20_153936_create_dynamic_values_table.php
+++ database/migrations/2022_09_20_153936_create_dynamic_values_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('key');
             $table->json('value');
+            $table->string('name');
             $table->tinyInteger('status')->default(1);
             $table->softDeletes();
             $table->timestamps();
