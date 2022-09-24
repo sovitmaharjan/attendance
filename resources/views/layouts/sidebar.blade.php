b6d82bafeb6ef44dd12f2fa923c688ff1ff168ed
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-09-24 16:29:02 +0545

    working on sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,7 @@
 @php
 $route = request()->route()->uri;
+$permissionNav = Request::is('permission*');
+$roleNav = Request::is('role*');
 @endphp
 
 <div id="kt_aside" class="aside aside-dark aside-hoverable" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_mobile_toggle">
@@ -20,7 +22,7 @@ $route = request()->route()->uri;
         <div class="hover-scroll-overlay-y my-5 my-lg-5" id="kt_aside_menu_wrapper" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer" data-kt-scroll-wrappers="#kt_aside_menu" data-kt-scroll-offset="0">
             <div class="menu menu-column menu-title-gray-800 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500" id="#kt_aside_menu" data-kt-menu="true" data-kt-menu-expand="false">
                 <div class="menu-item">
-                    <a class="menu-link" href="{{ route('dashboard') }}">
+                    <a class="menu-link @yield('dashboard')" href="{{ route('dashboard') }}">
                         <span class="menu-icon">
                             <span class="svg-icon svg-icon-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -39,7 +41,7 @@ $route = request()->route()->uri;
                         <div class="separator mx-1 my-4"></div>
                     </div>
                 </div>
-                <div data-kt-menu-trigger="click" class="menu-item here show menu-accordion">
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion {{ $permissionNav || $roleNav ? 'here show' : '' }}">
                     <span class="menu-link">
                         <span class="menu-icon">
                             <span class="svg-icon svg-icon-2">
@@ -54,7 +56,7 @@ $route = request()->route()->uri;
                         <span class="menu-arrow"></span>
                     </span>
                     <div class="menu-sub menu-sub-accordion menu-active-bg">
-                        <div data-kt-menu-trigger="click" class="menu-item here show menu-accordion">
+                        <div data-kt-menu-trigger="click" class="menu-item {{ $permissionNav ? 'here show' : '' }} menu-accordion">
                             <span class="menu-link">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
@@ -64,7 +66,7 @@ $route = request()->route()->uri;
                             </span>
                             <div class="menu-sub menu-sub-accordion menu-active-bg">
                                 <div class="menu-item">
-                                    <a class="menu-link active" href="{{ route('permission-group.index') }}">
+                                    <a class="menu-link @yield('permission_group')" href="{{ route('permission-group.index') }}">
                                         <span class="menu-bullet">
                                             <span class="bullet bullet-dot"></span>
                                         </span>
@@ -72,7 +74,7 @@ $route = request()->route()->uri;
                                     </a>
                                 </div>
                                 <div class="menu-item">
-                                    <a class="menu-link" href="{{ route('permission.index') }}">
+                                    <a class="menu-link @yield('permission')" href="{{ route('permission.index') }}">
                                         <span class="menu-bullet">
                                             <span class="bullet bullet-dot"></span>
                                         </span>
@@ -82,7 +84,7 @@ $route = request()->route()->uri;
                             </div>
                         </div>
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('role.index') }}">
+                            <a class="menu-link @yield('role')" href="{{ route('role.index') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
