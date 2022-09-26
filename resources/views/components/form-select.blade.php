cc23d79d34f09e8f17c7e408903901a78ea6eb7f
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-09-26 21:19:57 +0545

    select2

--- resources/views/components/form-select.blade.php
+++ resources/views/components/form-select.blade.php
@@ -2,7 +2,8 @@
 
 <div class="mb-10 fv-row">
     <label class="{{$class ?? 'form-label'}}">{{$label ?? $name}}</label>
-    <select name="{{$name}}" class="form-control mb-2">
+    <select name="{{$name}}" class="form-control mb-2" data-control="select2"
+            data-hide-search="false" data-placeholder="Select Role">
         {{$slot}}
     </select>
     <div class="text-muted fs-7">{{$info ?? ''}}
