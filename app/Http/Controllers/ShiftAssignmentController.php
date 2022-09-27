e319583584ae651287ab6a35d8c2993b4757e739
sovitmaharjan <sovitmaharjan@gmail.com>
2022-09-27 17:39:38 +0545

    shift assignment store finish

--- app/Http/Controllers/ShiftAssignmentController.php
+++ app/Http/Controllers/ShiftAssignmentController.php
@@ -6,8 +6,13 @@ use App\Http\Requests\ShiftAssignmentRequest;
 use App\Models\Branch;
 use App\Models\Department;
 use App\Models\Shift;
+use App\Models\ShiftAssignment;
 use App\Models\User;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ShiftAssignmentController extends Controller
 {
@@ -20,10 +25,34 @@ class ShiftAssignmentController extends Controller
         return view('shift-assignment.index', $data);
     }
 
-    public function store(ShiftAssignmentRequest $request) {
-        dd($request->all());
-        foreach($request->shift_repeater as $item) {
-            
+    public function store(ShiftAssignmentRequest $request)
+    {
+        // dd($request->all());
+        try {
+            DB::beginTransaction();
+            foreach ($request->shift_repeater as $item) {
+                $dates = CarbonPeriod::create($item['from_date'], $item['to_date']);
+                foreach ($dates as $date) {
+                    ShiftAssignment::firstOrCreate(
+                        [
+                            'shift_id' => $item['shift'],
+                            'employee_id' => $request->employee,
+                            'date' => $date
+                        ],
+                        [
+                            'extra' => [
+                                'nep_from_date' => $item['nep_from_date'],
+                                'nep_to_date' => $item['nep_to_date']
+                            ]
+                        ]
+                    );
+                }
+            }
+            DB::commit();
+            return back()->with('success', 'Shift has been assigned');
+        } catch (Exception $e) {
+            DB::rollBack();
+            return back()->with('error', $e->getMessage());
         }
     }
 }
--- app/Models/ShiftAssignment.php
+++ app/Models/ShiftAssignment.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class ShiftAssignment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'shift_id',
+        'employee_id',
+        'in_time',
+        'out_time',
+        'date',
+        'extra'
+    ];
+
+    protected $casts = [
+        'date' => 'datetime',
+        'extra' => 'array',
+    ];
 }
