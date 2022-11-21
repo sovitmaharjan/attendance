99792c9dc034395300d0b15aaab609ef20a16229
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-11-21 11:56:05 +0545

    working on employee_substitute_day

---
+++ app/Http/Controllers/EmployeeSubstituteDayController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EmployeeSubstituteDayRequest;
+use Illuminate\Http\Request;
+
+class EmployeeSubstituteDayController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(EmployeeSubstituteDayRequest $request)
+    {
+
+    }
+}
