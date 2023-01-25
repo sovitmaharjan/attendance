7069eef18910634d5b26492d26cf4c8bc3e517e3
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2023-01-25 17:04:44 +0545

    Employee Validation

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -14,26 +14,27 @@ class EmployeeRequest extends FormRequest
     public function rules()
     {
         return [
-            'prefix' => 'required',
-            'firstname' => 'required',
-            'middlename' => 'nullable',
-            'lastname' => 'required',
-            'email' => 'required',
-            'phone' => 'required',
-            'address' => 'required',
-            'gender' => 'required',
-            'marital_status' => 'required',
-            'dob' => 'required',
-            'nepali_dob' => 'nullable',
-            'join_date' => 'required',
-            'nepali_join_date' => 'nullable',
-            'company_id' => 'required',
-            'branch_id' => 'nullable',
-            'department_id' => 'nullable',
-            'login_id' => 'required',
-            'supervisor_id' => 'nullable',
-            'status' => 'required',
-            'type' => 'required'
+            'prefix' => ['required'],
+            'firstname' => ['required'],
+            'middlename' => ['nullable'],
+            'lastname' => ['required'],
+            'email' => ['required', 'unique:users,email,'.$this->id],
+            'phone' => ['required', 'numeric'],
+            'address' => ['required'],
+            'gender' => ['required', 'in:Male,Female,Other'],
+            'marital_status' => ['required'],
+            'dob' => ['required', 'date', 'date_format:Y-m-d'],
+            'nepali_dob' => ['nullable'],
+            'join_date' => ['required', 'date', 'date_format:Y-m-d'],
+            'nepali_join_date' => ['nullable'],
+            'company_id' => ['required'],
+            'branch_id' => ['nullable'],
+            'department_id' => ['nullable'],
+            'login_id' => ['required'],
+            'supervisor_id' => ['nullable'],
+            'status' => ['required'],
+            'type' => ['required'],
+            'image' => ['required', 'image', 'mimes:jpg,jpeg,png', 'max:5120']
         ];
     }
 }
